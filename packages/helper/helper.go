45e516d42001435c591a23d9d503d785c0328199
Gabriel Schreiner Bueno de Camargo <gsbcamargo@gmail.com>
2023-06-26 23:51:17 -0300

    adds helper package to test import and modifies print to println

---
+++ packages/helper/helper.go
@@ -0,0 +1,7 @@
+package helper
+
+import "fmt"
+
+func Write() {
+	fmt.Println("I'm inside a helper package")
+}
---
+++ packages/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"packages/helper"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+	helper.Write()
+}
