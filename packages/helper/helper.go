3e509b64e5e81bc7c3305c5b7d37b88ca77205af
Gabriel Schreiner Bueno de Camargo <gsbcamargo@gmail.com>
2023-06-27 00:07:32 -0300

    fixes grammar

--- packages/helper/helper.go
+++ packages/helper/helper.go
@@ -3,5 +3,10 @@ package helper
 import "fmt"
 
 func Write() {
-	fmt.Println("I'm inside a helper package")
+	fmt.Println("I'm inside a helper package.")
+	privateWrite()
+}
+
+func privateWrite() {
+	fmt.Println("Hi! I'm private! This function cannot be accessed outside of the 'helper' package.")
 }
