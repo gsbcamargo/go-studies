40a9223b81287e209327b9748182661dd5e4827f
Gabriel Schreiner Bueno de Camargo <gsbcamargo@gmail.com>
2023-06-27 23:03:19 -0300

    tests external dependency

--- packages/go.mod
+++ packages/go.mod
@@ -1,3 +1,5 @@
 module packages
 
 go 1.20
+
+require github.com/badoux/checkmail v1.2.1 // indirect
--- packages/main.go
+++ packages/main.go
@@ -3,9 +3,17 @@ package main
 import (
 	"fmt"
 	"packages/helper"
+
+	"github.com/badoux/checkmail"
+	// go mod tidy -> to remove unused dependencies
 )
 
 func main() {
 	fmt.Println("Hello, World!")
 	helper.Write()
+
+	correct := checkmail.ValidateFormat("gabriel@gabriel.dev")
+	wrongFormat := checkmail.ValidateFormat("1234")
+	fmt.Println(correct)
+	fmt.Println(wrongFormat)
 }
