6d60ea2b289b4f6dbbe6dd47841d3be5cef1fc63
Gabriel Schreiner Bueno de Camargo <gsbcamargo@gmail.com>
2023-07-06 22:14:02 -0300

    adds struct without initialization

--- structs/structs.go
+++ structs/structs.go
@@ -4,6 +4,7 @@ import "fmt"
 
 func main() {
 	var user1 user
+	fmt.Printf("Struct variable without initialization: %v\n", user1) // each field is given it's default value (empty string and 0, in this scenario)
 	user1.name = "Gabriel"
 	fmt.Printf("User name: %s", user1.name)
 }
